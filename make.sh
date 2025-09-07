90e07c6dd5e26fbc9a459d190ff627f79d3d24e6
Emmitt <emmittluhning@gmail.com>
2025-09-07 19:39:28 -0400

    Paint to window (#2)
    * function declaration for bitmap copy
    
    * tiny amount of work on bit bitbit
    
    * paint bitmap to window and handle basic resize
    
    * started to specify the client API

--- make.sh
+++ make.sh
@@ -1,1 +1,6 @@
-x86_64-w64-mingw32-gcc renderer.c -o renderer.exe -lgdi32
+x86_64-w64-mingw32-gcc renderer.c -o renderer.exe -g -lgdi32 
+x86_64-w64-mingw32-gcc client.c -o client.exe -g -lgdi32 
+cp renderer.exe /mnt/c/msys64/home/emmit/renderer.exe
+cp renderer.c /mnt/c/msys64/home/emmit/renderer.c
+cp client.exe /mnt/c/msys64/home/emmit/client.exe
+cp client.c /mnt/c/msys64/home/emmit/client.c
